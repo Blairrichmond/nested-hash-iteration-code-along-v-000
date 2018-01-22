c31b6360dbabf834ce5ea278cb788a357d6c4af4
Brent  Ransom <sadumedusa@gmail.com>
2018-01-22 15:40:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        value.delete_if {|flavor| flavor == "strawberry"}
 end
